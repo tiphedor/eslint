2e0cd86fd9563fae4e761d87d433e7f7fa49958b
Martin STEFFEN <mail@tiph.io>
2021-11-22 21:19:40 +0100

    added basic config

--- packages/base/index.js
+++ packages/base/index.js
@@ -1,1 +1,16 @@
-dfwfwtest
+module.exports = {
+    extends: [
+        'eslint:recommended',
+    ],
+    rules: {
+        indent: [ 'error', 'tab' ],
+        'linebreak-style': [ 'error', 'unix' ],
+        quotes: [ 'error', 'single' ],
+        semi: [ 'error', 'always' ],
+        'quote-props': [ 'error', 'as-needed' ],
+        'array-bracket-spacing': [ 'error', 'always' ],
+        'object-curly-spacing': [ 'error', 'always' ],
+        'key-spacing': [ 'error', { beforeColon: false } ],
+        'comma-spacing': [ 'error', { before: false, after: true } ],
+    }
+};
